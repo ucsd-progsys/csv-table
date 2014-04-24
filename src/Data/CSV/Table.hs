be04adc0f913c81796d4c870c9f9c99b7a04b190
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-04-24 10:14:05 -0700

    ASDASD

--- src/Data/CSV/Table.hs
+++ src/Data/CSV/Table.hs
@@ -1,5 +1,10 @@
-module Data.CSV.Table where
+module Data.CSV.Table (
+  module Data.CSV.Table.Types,
+  module Data.CSV.Table.Ops,
+  module Data.CSV.Table.Email 
+  ) where
 
 import Data.CSV.Table.Types
 import Data.CSV.Table.Ops
-import Data.CSV.Table.Email
+import Data.CSV.Table.Email 
+  
